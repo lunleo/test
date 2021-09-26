34522067a006afd26f27482d641acb1a94791d9c
lunleo <luna.lenya@gmail.com>
2021-09-26 17:01:30 +0300

    Changed Java

--- src/TestApp.java
+++ src/TestApp.java
@@ -1,5 +1,8 @@
 public class TestApp {
     public static void main(String[] args) {
+        float a=10;
+        float b=(float) 12.5;
+        System.out.println("a+b="+(a+b));
         System.out.println("Git Test Application!");
     }
 }
