598e7603ba7d6aac1f29be22ac81ce8f88695ac5
lunleo <luna.lenya@gmail.com>
2021-09-26 17:04:31 +0300

    Changed Java

--- src/TestApp.java
+++ src/TestApp.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 public class TestApp {
     public static void main(String[] args) {
-        float a=10;
-        float b=(float) 12.5;
+        Scanner in = new Scanner(System.in);
+        System.out.println("1st num: ");
+        double a = Double.valueOf(in.nextLine());
+
+        System.out.println("2nd num: ");
+        double b = Double.valueOf(in.nextLine());
+
         System.out.println("a+b="+(a+b));
         System.out.println("Git Test Application!");
     }
